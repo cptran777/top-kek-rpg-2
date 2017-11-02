3b6ea9285ff8ed8ab080a54eaac8964a374ab3dd
cptran777 <cptran14@gmail.com>
2017-11-02 14:03:00 -0700

    Hook up main container to the store and index of application

--- top-kek-rpg-client/src/index.tsx
+++ top-kek-rpg-client/src/index.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import Main from './containers/Main';
 import registerServiceWorker from './registerServiceWorker';
+import { testReducer } from './reducers/index';
+import { StoreState } from './types/index';
+
+const store = createStore<StoreState>(testReducer, {
+  testState: 'hello'
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Main />  
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
